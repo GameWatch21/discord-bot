88521e2f5fbbc0aae55c5386af94050f7e9ebd92
GameWatch21 <43901265+GameWatch21@users.noreply.github.com>
2021-07-05 21:57:47 +0700

    Just guessing
    Dont pull

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ require("./handler/Event.js")(client);
 require('dotenv').config();
 client.on("warn", console.warn);
 client.on("error", console.error);
-client.login(process.env.SECRET).catch(console.error);
+client.login(process.env['SECRET']).catch(console.error);
