0a45c38ce0f803a50c9a8d06e7512e30dad8a690
GameWatch21 <43901265+GameWatch21@users.noreply.github.com>
2021-07-05 22:04:21 +0700

    Just another type

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const client = new Discord.Client();
 // const idkman = require("./handler/Client.js");
 // const client = new idkman();
 
+// This is just another type to use .env
+const token = process.env.SECRET;
+
 require("./handler/Module.js")(client);
 require("./handler/Event.js")(client);
 require('dotenv').config();
 client.on("warn", console.warn);
 client.on("error", console.error);
-client.login(process.env.SECRET).catch(console.error);
+client.login(token).catch(console.error);
+// or you can use client.login(process.env.SECRET).catch(console.error);
